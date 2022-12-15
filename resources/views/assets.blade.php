6d37717a92138535b0d4067ffb2969e7e07a89dc
Hermange milang <pgemilang55@gmail.com>
2022-12-16 04:31:25 +0700

    buat database order sama assets sekalian view nya assets dikit

--- resources/views/assets.blade.php
+++ resources/views/assets.blade.php
@@ -2,15 +2,43 @@
 
 @section('main-content')
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">{{ __('Order') }}</h1>
+    <h1 class="h3 mb-4 text-gray-800 cl font-weight-bold">{{ __('Assets') }}</h1>
 
     <div class="row justify-content-center">
 
         <div class="col-lg-8">
-            <h1>Ini Halaman Assets</h1>
+
+            <div class="card shadow mb-4">
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="form-group focused">
+                                <label class="form-control-label" for="foto">foto</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="form-group focused">
+                                <label class="form-control-label" for="jenis barang">Jenis Barang</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="form-group focused">
+                                <label class="form-control-label" for="harga">Harga</label>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="form-group focused">
+                                <label class="form-control-label" for="stock">Stock</label>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+                    
+                </div>
+            </div>
 
         </div>
 
     </div>
-
 @endsection
